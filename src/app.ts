5c43fd2d0e0b6318b58bd7399cbf2fb6d086d743
badmen007 <1572226525@qq.com>
2023-08-14 21:16:16 +0800

    feat: node 服务基本搭建

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import Koa from 'koa'
+import cors from '@koa/cors'
+import bodyparser from 'koa-bodyparser'
+import logger from 'koa-logger'
+import authRoutes from './routes/auth'
+
+const app = new Koa()
+
+app.use(cors())
+app.use(
+  bodyparser({
+    // 解析请求体
+    enableTypes: ["json", "form", "text"],
+  })
+);
+
+app.use(logger())
+
+app.use(authRoutes.routes()).use(authRoutes.allowedMethods())
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`server listening on ${port}`)
+})
+app.on('error', err => console.log('server error', err))
