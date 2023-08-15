c9b9673f05e69d2a5a0c8acc232dba588973e70f
badmen007 <1572226525@qq.com>
2023-08-15 23:47:36 +0800

    feat: 自定义 401 错误

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import Koa from "koa";
 import cors from "@koa/cors";
 import bodyparser from "koa-bodyparser";
 import logger from "koa-logger";
+import jwt from "koa-jwt";
 import authRoutes from "./routes/auth";
+import { jwtSecret } from "./config/auth";
 import "./db";
 
 const app = new Koa();
@@ -16,6 +18,28 @@ app.use(
 );
 
 app.use(logger());
+
+// 自定义401错误
+app.use((ctx, next) => {
+  return next().catch((err) => {
+    if (err.status === 401) {
+      ctx.status = 401;
+      ctx.body = {
+        code: 401,
+        error: "未登录 token失效",
+      };
+    } else {
+      ctx.throw(err);
+    }
+  });
+});
+// token验证 header未携带token 直接返回401 Authentication Error
+app.use(
+  jwt({ secret: jwtSecret }).unless({
+    // 白名单
+    path: ["/api/auth/login", "/api/auth/register"],
+  })
+);
 
 app.use(authRoutes.routes()).use(authRoutes.allowedMethods());
 
